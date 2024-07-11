15e89599990f054ef6b244de1f5073b4ae265d12
DrDavidN <128225763+DrDavidN@users.noreply.github.com>
2024-07-11 10:50:34 +0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1gfpapk9t5k4ev5ug0p"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gg65gm92a1t1aubju3"
+}
+
+variable "a-zone" {
+  default = "ru-central1-c"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "centos-7-base" {
+  default = "fd88mqi0d02c7jbkest0"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
